1993dd4a81766795cfb259d19b3a412da586a23f
Justin Carl de Guia <justincarlcdeguia@iskolarngbayan.pup.edu.ph>
2021-11-13 12:22:11 +0800

    added age evaluations to show user their life stage

--- lifeStages.py
+++ lifeStages.py
@@ -8,11 +8,24 @@ def showLifeStages():
     print("Ages 0-12: Kid")
     print("Ages 13-17: Teen")
     print("Age 18: Debut")
-    print("Ages 19+: Adult")
+    print("Ages 19+: Adult\n")
 
 def userAsk():
+    global age
     age = int(input("What is your age?: "))
     return age
 
 showLifeStages()
 age = userAsk()
+
+#Step 2: Prepare the conditions and show the output.
+def evalAge(age):
+    if age <= 12:
+        print(f"You are {age} years old and a kid! Have fun!")
+    elif age >= 13 and age <= 17:
+        print(f"You are {age} years old and a teen! Good luck!")
+    elif age == 18:
+        print(f"You are {age} years old! Congratulations on your debut!")
+    else:
+        print(f"You are {age} years old and an adult! Good luck on your responsibilities!")
+evalAge(age)
