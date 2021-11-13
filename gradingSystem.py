160ed3ed32cf2b9500dd7104a5a2c6cf37149759
Justin Carl de Guia <justincarlcdeguia@iskolarngbayan.pup.edu.ph>
2021-11-13 11:35:32 +0800

    fixed None in table and added equivalent conditions

--- gradingSystem.py
+++ gradingSystem.py
@@ -18,5 +18,35 @@ def gradeTable():
     print("    5.0     |          65-74        |      Failure   ")
     print("    Inc.    |                       |    Incomplete  ")
     print("     W      |                       |     Withdrawn  ")
-    print("     D      |                       |      Dropped   ")
-print(gradeTable())
+    print("     D      |                       |      Dropped   \n")
+
+#Step 2: Let the user input their grade.
+def gradeInput():
+    gradeInput = float(input("Input Grade: "))
+    return gradeInput
+
+#Step 3: Set up display conditions for equivalence
+def equiGrade():
+    if gUser >= 97 and gUser <= 100:
+        print("Grade/Mark: 1.0")
+    elif gUser >= 94 and gUser <= 96:
+        print("Grade/Mark: 1.25")
+    elif gUser >= 91 and gUser <= 93:
+        print("Grade/Mark: 1.5")
+    elif gUser >= 88 and gUser <= 90:
+        print("Grade/Mark: 1.75")
+    elif gUser >= 85 and gUser <= 87:
+        print("Grade/Mark: 2.0")
+    elif gUser >= 82 and gUser <= 84:
+        print("Grade/Mark: 2.25")
+    elif gUser >= 79 and gUser <= 81:
+        print("Grade/Mark: 2.5")
+    elif gUser >= 76 and gUser <= 78:
+        print("Grade/Mark: 2.75")
+    elif gUser == 75:
+        print("Grade/Mark: 3.0")
+    else:
+        pass
+gradeTable()
+gUser = gradeInput()
+equiGrade()
