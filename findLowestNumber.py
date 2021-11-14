67095c8cefc06414a663eec3dbf62f8d3a9d3202
Justin Carl de Guia <justincarlcdeguia@iskolarngbayan.pup.edu.ph>
2021-11-14 16:51:45 +0800

    replaced int with float

--- findLowestNumber.py
+++ findLowestNumber.py
@@ -5,9 +5,9 @@
 #Step 1: Ask for user inputs and assign them to values.
 print("Input three (3) numbers and the program will find the lowest number.\n")
 def userNumbers():
-    a = int(input("Input the first number: "))
-    b = int(input("Input the second number: "))
-    c = int(input("Input the third number: "))
+    a = float(input("Input the first number: "))
+    b = float(input("Input the second number: "))
+    c = float(input("Input the third number: "))
     return a, b, c
 a, b, c = userNumbers()
 
